f1fd7f8d03c02156471abf71e668bef848e2b556
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-08-01 19:14:13 +0900

    fastlms 초기화 설정-2

--- src/main/java/com/zerobase/fastlms/ServletInitializer.java
+++ src/main/java/com/zerobase/fastlms/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    // 2023.08.01
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(FastlmsApplication.class);
